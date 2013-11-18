a88a265f713817592be380c3ee7af5149f5db853
Fraz <frazer.irving@mailonline.co.uk>
2013-11-18 16:54:29 +0000

    clean up

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject clj-orphanage "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-orphanage "0.1.0"
+  :description "Find orphaned clojure files"
+  :url "https://github.com/clumsyjedi/clj-orphanage"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.reader "0.7.10"]
                  [org.clojure/tools.namespace "0.2.4"]
-
                  ])
